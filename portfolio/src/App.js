5db10b9642258e5db8d3bae129547e683f55b23a
Bradford-Lowry <brad@nnexe.com>
2021-07-03 12:40:50 +0200

    React inital build
    react initial build

---
+++ portfolio/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<nav>
+  <ul>
+    <li>home</li>
+  </ul>
+</nav>
+    </div>
+  );
+}
+
+export default App;
